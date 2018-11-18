2bcf7def0777903762f88cc77836d7ea567b06c1
foureverhh <foureverhh@gmail.com>
2018-11-18 19:09:10 +0100

    changed the fragments to Frag1 , Frag2 and Frag3

--- app/src/main/java/com/nackademin/foureverhh/tabview181118/MainActivity.java
+++ app/src/main/java/com/nackademin/foureverhh/tabview181118/MainActivity.java
@@ -3,6 +3,7 @@ package com.nackademin.foureverhh.tabview181118;
 import android.support.design.widget.TabLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
@@ -137,9 +138,24 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public Fragment getItem(int position) {
+            Fragment fragment = null;
+            switch (position){
+                case 0:
+                    fragment = new Frag1();
+                    break;
+                case 1:
+                    fragment = new Frag2();
+                    break;
+                case 2:
+                    fragment = new Frag3();
+                    break;
+            }
+            /*
             // getItem is called to instantiate the fragment for the given page.
             // Return a PlaceholderFragment (defined as a static inner class below).
             return PlaceholderFragment.newInstance(position + 1);
+            */
+            return fragment;
         }
 
         @Override
